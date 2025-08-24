59da6731be125c99112c5b767b1688297ff952c5
ClementinoChieque <chieque67@gmail.com>
2025-08-24 22:56:39 +0100

    Update QuantificationInfo.tsx

--- src/components/features/quantify/QuantificationInfo.tsx
+++ src/components/features/quantify/QuantificationInfo.tsx
@@ -8,8 +8,8 @@ interface QuantificationInfoProps {
 const QuantificationInfo: React.FC<QuantificationInfoProps> = ({ isMobile }) => {
   return (
     <div className="text-[10px] md:text-sm text-muted-foreground text-center mt-2 md:mt-4">
-      <p>A quantificação é um processo que analisa o mercado em tempo real.</p>
-      {!isMobile && <p>Torna as decisões de investimento mais precisas e automáticas.</p>}
+      <p></p>
+      {!isMobile && <p></p>}
     </div>
   );
 };
