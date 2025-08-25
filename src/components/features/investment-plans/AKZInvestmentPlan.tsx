d8c50d71626b221f60a788a9a63fd1d5e7e91bb5
ClementinoChieque <chieque67@gmail.com>
2025-08-25 23:49:03 +0100

    Update AKZInvestmentPlan.tsx

--- src/components/features/investment-plans/AKZInvestmentPlan.tsx
+++ src/components/features/investment-plans/AKZInvestmentPlan.tsx
@@ -15,10 +15,10 @@ const AKZInvestmentPlan: React.FC = () => {
   const isMobile = useIsMobile();
   
   const investmentData = [
-    { level: 'BitcoinL1', investment: '4 000,00 Kz', dailyEarning: '800,00 Kz', monthlyEarnings: '30 000,00 Kz', yearlyEarnings: '91 250,00 Kz' },
-    { level: 'BitcoinL2', investment: '10 000,00 Kz', dailyEarning: '1200,00 Kz', monthlyEarnings: '46 000,00 Kz', yearlyEarnings: '273 750,00 Kz' },
-    { level: 'BitcoinL3', investment: '50 000,00 Kz', dailyEarning: '2500,00 Kz', monthlyEarnings: '75 000,00 Kz', yearlyEarnings: '638 750,00 Kz' },
-    { level: 'BitcoinL4', investment: '100 000,00 Kz', dailyEarning: '7000,00 Kz', monthlyEarnings: '210 000,00 Kz', yearlyEarnings: '730 000,00 Kz' },
+    { level: 'BitcoinL1', investment: '4 000,00 Kz', daily: '800,00 Kz', monthlyEarnings: '30 000,00 Kz', yearlyEarnings: '91 250,00 Kz' },
+    { level: 'BitcoinL2', investment: '10 000,00 Kz', daily: '1200,00 Kz', monthlyEarnings: '46 000,00 Kz', yearlyEarnings: '273 750,00 Kz' },
+    { level: 'BitcoinL3', investment: '50 000,00 Kz', daily: '2500,00 Kz', monthlyEarnings: '75 000,00 Kz', yearlyEarnings: '638 750,00 Kz' },
+    { level: 'BitcoinL4', investment: '100 000,00 Kz', daily: '7000,00 Kz', monthlyEarnings: '210 000,00 Kz', yearlyEarnings: '730 000,00 Kz' },
 
   ];
 
@@ -74,7 +74,7 @@ const AKZInvestmentPlan: React.FC = () => {
                 <TableHead className="text-white font-bold">Ganhos diários</TableHead>
                 <TableHead className="text-white font-bold">Ganhos Mensais</TableHead>
                 <TableHead className="text-white font-bold">365 Dias</TableHead>
-                <TableHead className="text-white font-bold">365 Dias</TableHead>
+               
               </TableRow>
             </TableHeader>
             <TableBody>
